84e49535121163036416be86471d066977be7116
Andrew Hunt <andrewhunt@outlook.com>
2019-03-29 21:19:49 -0400

    Upgrade to .NET Standard 2.0

---
+++ FileDownloader.Standard/DownloadFileCompletedArgs.cs
@@ -0,0 +1,105 @@
+﻿//----------------------------------------------------------------------------------------------------
+// <copyright company="Avira Operations GmbH & Co. KG and its licensors">
+// © 2016 Avira Operations GmbH & Co. KG and its licensors.  All rights reserved.
+// </copyright>
+//----------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace FileDownloader
+{
+    /// <summary>
+    /// DownloadFileCompleted event args
+    /// </summary>
+    public class DownloadFileCompletedArgs : EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DownloadFileCompletedArgs"/> class
+        /// </summary>
+        /// <param name="state">State of download</param>
+        /// <param name="fileName">Local path to downloaded file</param>
+        /// <param name="fileSource">Downloaded file source</param>
+        /// <param name="downloadTime">Downloaded time</param>
+        /// <param name="bytesTotal">File size</param>
+        /// <param name="bytesReceived">Received bytes</param>
+        /// <param name="error">Exception object</param>
+        public DownloadFileCompletedArgs(CompletedState state, string fileName, Uri fileSource, TimeSpan downloadTime, long bytesTotal, long bytesReceived, Exception error)
+        {
+            State = state;
+            FileName = fileName;
+            FileSource = fileSource;
+            Error = error;
+            DownloadTime = downloadTime;
+            BytesTotal = bytesTotal;
+            BytesReceived = bytesReceived;
+        }
+
+        /// <summary>
+        /// Gets the download state 
+        /// </summary>
+        public CompletedState State { get; private set; }
+
+        /// <summary>
+        /// Gets the name of downloaded file
+        /// </summary>
+        public string FileName { get; private set; }
+
+        /// <summary>
+        /// Gets the download source
+        /// </summary>
+        public Uri FileSource { get; private set; }
+
+        /// <summary>
+        /// Gets the error, or null if there is no error
+        /// </summary>
+        public Exception Error { get; private set; }
+
+        /// <summary>
+        /// Gets the total download time
+        /// </summary>
+        public TimeSpan DownloadTime { get; private set; }
+
+        /// <summary>
+        /// Gets the number of received bytes
+        /// </summary>
+        public long BytesReceived { get; private set; }
+
+        /// <summary>
+        /// Gets the number of total bytes which should be received
+        /// </summary>
+        public long BytesTotal { get; private set; }
+
+        /// <summary>
+        /// Gets the download progress in percent, from 0 to 100
+        /// </summary>
+        public int DownloadProgress
+        {
+            get
+            {
+                if (BytesTotal <= 0 || BytesReceived <= 0)
+                {
+                    return 0;
+                }
+                return Convert.ToInt32((float)BytesReceived / BytesTotal * 100);
+            }
+        }
+
+        /// <summary>
+        /// Gets the download speed in kilobytes per second
+        /// </summary>
+        public int DownloadSpeedInKiloBytesPerSecond
+        {
+            get
+            {
+                if (DownloadTime == TimeSpan.Zero || BytesReceived == 0)
+                {
+                    return 0;
+                }
+
+                var kiloBytesReceived = BytesReceived / 1024.0;
+
+                return Convert.ToInt32(kiloBytesReceived / DownloadTime.TotalSeconds);
+            }
+        }
+    }
+}
---
+++ FileDownloader.Standard/Logging/LoggerFacade.cs
@@ -0,0 +1,115 @@
+﻿//----------------------------------------------------------------------------------------------------
+// <copyright company="Avira Operations GmbH & Co. KG and its licensors">
+// © 2016 Avira Operations GmbH & Co. KG and its licensors.  All rights reserved.
+// </copyright>
+//----------------------------------------------------------------------------------------------------
+
+using System;
+using System.Diagnostics;
+
+namespace FileDownloader.Logging
+{
+    internal class LoggerFacade : ILogger, IDisposable
+    {
+        private readonly TraceSource traceSource;
+
+        protected LoggerFacade(string name)
+        {
+            Name = name;
+            this.traceSource = new TraceSource("Avira.FileDownloader");
+        }
+
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Get a logger with a specified name.
+        /// </summary>
+        /// <param name="loggerName">The name of the logger.</param>
+        /// <returns>Instance of logger.</returns>
+        public static ILogger GetLogger(string loggerName)
+        {
+            return new LoggerFacade(loggerName);
+        }
+
+        /// <summary>
+        /// Get a logger with the name of the calling class.
+        /// </summary>
+        /// <returns>Instance of logger.</returns>
+        public static ILogger GetCurrentClassLogger()
+        {
+            string loggerName;
+            Type declaringType;
+            var framesToSkip = 1;
+            do
+            {
+                var frame = new StackFrame(framesToSkip, false);
+                var method = frame.GetMethod();
+                declaringType = method.DeclaringType;
+                if (declaringType == null)
+                {
+                    loggerName = method.Name;
+                    break;
+                }
+
+                framesToSkip++;
+                loggerName = declaringType.Name;
+            }
+            while (declaringType.Module.Name.Equals("mscorlib.dll", StringComparison.OrdinalIgnoreCase));
+
+            return GetLogger(loggerName);
+        }
+
+        public void Debug(string message, params object[] args)
+        {
+            TraceEvent(TraceEventType.Verbose, message, args);
+        }
+
+        public void Info(string message, params object[] args)
+        {
+            TraceEvent(TraceEventType.Information, message, args);
+        }
+
+        public void Warn(string message, params object[] args)
+        {
+            TraceEvent(TraceEventType.Warning, message, args);
+        }
+
+        public void Error(string message, params object[] args)
+        {
+            TraceEvent(TraceEventType.Error, message, args);
+        }
+
+        public void Fatal(string message, params object[] args)
+        {
+            TraceEvent(TraceEventType.Critical, message, args);
+        }
+
+        public void Dispose()
+        {
+            this.traceSource.Close();
+        }
+
+        private void TraceEvent(TraceEventType eventType, string message, params object[] args)
+        {
+            string formattedMessage;
+            if (args.Length > 0)
+            {
+                try
+                {
+                    formattedMessage = string.Format(message, args);
+                }
+                catch (FormatException)
+                {
+                    formattedMessage = message;
+                }
+            }
+            else
+            {
+                formattedMessage = message;
+            }
+
+            this.traceSource.TraceEvent(eventType, 0, string.Format("[{0}] {1}", Name, formattedMessage));
+            this.traceSource.Flush();
+        }
+    }
+}
